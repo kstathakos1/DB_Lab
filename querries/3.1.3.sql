81b3d255f7397e66046509daffd1d35c00a1dc2a
kstathakos1 <kstathakos@outlook.com>
2023-05-25 21:12:43 +0300

    3.1.3

--- querries/3.1.3.sql
+++ querries/3.1.3.sql
@@ -1,7 +1,7 @@
 SELECT concat(u.first_name,' ',u.last_name) AS Name_teacher, count(r.rental_id) as Number_rented_Books
 FROM user u
 inner join rental r on u.username = r.username
-where YEAR(CURRENT_DATE)-year(u.birth_date)<=40 AND u.status='teacher'
+where YEAR(CURRENT_DATE)-year(u.birth_date)<=40 AND (u.status='teacher' or u.status='operator')
 group by u.username
 order by count(r.rental_id) desc
 ;
