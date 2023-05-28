806141557ea72ba02dc5d2675fdbda36344d5684
pgkoulta <voulagkoulta02@gmail.com>
2023-05-28 15:00:26 +0300

    Make styling changes

--- school_library/registration.php
+++ school_library/registration.php
@@ -18,14 +18,11 @@
       <!-- Import css and js packages -->
       <link rel="stylesheet" href="css/bootstrap.min.css">
       <link rel="stylesheet" type="text/css" href="css/custom.css?<?=time()?>">
-
       <link rel="stylesheet" href="css/fontawesome.min.css">
       <link rel="stylesheet" href="css/all.min.css">
 
       <script src="js/jquery.min.js"></script>
       <script src="js/bootstrap.min.js"></script>
-
-
       <link rel="stylesheet" href="css/jquery-ui.css">
       <script src="js/jquery-1.10.2.js"></script>
       <script src="js/jquery-ui.js"></script>
