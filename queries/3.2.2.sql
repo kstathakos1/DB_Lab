925e03278a62f0e06b29f78d6bf3d51bc9994a9a
stavrakas13 <128683063+stavrakas13@users.noreply.github.com>
2023-06-03 01:29:27 +0300

    Update 3.2.2.sql

--- queries/3.2.2.sql
+++ queries/3.2.2.sql
@@ -1,5 +1,5 @@
 DELIMITER $$
-CREATE PROCEDURE out_of_date_borrowers (IN first_name CHAR(255), IN last_name CHAR(255), IN days_out INT,IN school int)
+CREATE PROCEDURE out_of_date_borrowers (IN first_name CHAR(255), IN last_name CHAR(255), IN days_out INT, IN school INT)
 BEGIN
     SELECT CONCAT(u.last_name, ' ', u.first_name) AS 'Borrower, out of date', 
     DATEDIFF(NOW(), r.expected_return_date) AS 'delaying_time'
@@ -10,7 +10,8 @@ BEGIN
            OR (days_out IS NOT NULL AND days_out >= 7 AND DATEDIFF(NOW(), r.expected_return_date) > days_out))
     AND (u.first_name = first_name OR first_name IS NULL OR first_name = '')
     AND (u.last_name = last_name OR last_name IS NULL OR last_name = '')
-    AND (u.school_id=school)
+    AND (u.school_id=school OR school IS NULL)
     GROUP BY u.last_name, u.first_name;
 END$$
 DELIMITER ;
+
